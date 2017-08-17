19d378a45c28d9303b47e56b4c653fb77de62536
marcioz98 <marciozgaming@gmail.com>
2017-08-17 21:36:12 +0200

    Initial upload

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,45 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel exampleModel = loader.loadToVAO(vertices);
+		
+		while(!Display.isCloseRequested()) {
+			
+			renderer.prepare();
+			renderer.render(exampleModel);
+			
+			DisplayManager.updateDisplay();
+
+		}
+		
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+
+	}
+
+}
