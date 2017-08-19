030b9c9ab422cf929bf978646b833848f43b89fc
marcioz98 <marciozgaming@gmail.com>
2017-08-19 19:50:18 +0200

    Added test

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -28,11 +28,11 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		
-		ModelData sample = OBJFileLoader.loadOBJ("yasuo");
+		ModelData sample = OBJFileLoader.loadOBJ("cube");
 		RawModel sampleModel = loader.loadToVAO(sample.getVertices(), sample.getTextureCoords(), sample.getNormals(), sample.getIndices());
-		ModelTexture texture = new ModelTexture(loader.loadTexture("yasuo"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("face"));
 		texture.setShineDamper(25);
-		texture.setReflectivity(0.6f);
+		texture.setReflectivity(0.8f);
 		TexturedModel texturedModel = new TexturedModel(sampleModel, texture);
 		
 		ArrayList<Entity> entities = new ArrayList<Entity>();
@@ -41,19 +41,19 @@ public class MainGameLoop {
 		float distance = 100f;
 		for(int i = 1; i < 30; i++) {
 			for(int j = 1; j < 30; j++) {
-					entities.add(new Entity(texturedModel, new Vector3f(i*distance, 0, j*distance), 0, Maths.randFloat(0.0f, 360.0f), 0, 1));
+					entities.add(new Entity(texturedModel, new Vector3f(i*distance, 0, j*distance), 0, Maths.randFloat(0.0f, 360.0f), 0, 25));
 			}
 		}
 		
-		// Entity entity = new Entity(texturedModel, new Vector3f(0, 0, 0), 0, 0, 0, 1f);
+		entities.add(new Entity(texturedModel, new Vector3f(1500, 0, 1500), 0, Maths.randFloat(0.0f, 360.0f), 0, 250));
 		
-		Light light = new Light(new Vector3f(1500, 1000, 1500), new Vector3f(1f, 1f, 1f)); // x, intensity, "y"
+		Light light = new Light(new Vector3f(1500, 2000, 1500), new Vector3f(1f, 1f, 1f)); // x, intensity, "y"
 		
 		Camera camera = new Camera();
 		
 		List<Terrain> terrains = new ArrayList<Terrain>();
 		
-		ModelTexture terrainTexture = new ModelTexture(loader.loadTexture("face"));
+		ModelTexture terrainTexture = new ModelTexture(loader.loadTexture("green"));
 		
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
@@ -72,6 +72,7 @@ public class MainGameLoop {
 			}
 			
 			for(Entity entity : entities) {
+				entity.rotateEntity(0, Maths.randFloat(2f, 5f), 0);
 				renderer.processEntity(entity);
 			}
 			
